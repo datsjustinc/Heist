d32d541b22d69fd5a85800330fff9469a4d943bb
datsjustinc <jc9957@nyu.edu>
2022-05-04 19:46:25 -0400

    Implement world volume saturation script.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,18 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 public class GameManager : MonoBehaviour
 {
     public GameObject outsideVolume;
+    public GameObject insideVolume;
     public Volume volume;
+    public ColorAdjustments color;
+    public readonly float startSaturation = 75;
+    public readonly float endSaturation = -75;
+
+    public bool coroutineRun = false;
 
     void Start()
     {
         volume = outsideVolume.GetComponent<Volume>();
+        volume.profile.TryGet<ColorAdjustments>(out color);
     }
 
     void Update()
     {
+        if (insideVolume.activeSelf && !coroutineRun && (color.saturation.value > endSaturation))
+        {
+            StartCoroutine(ChangeWorld());
+        }
+    }
 
+    IEnumerator ChangeWorld()
+    {
+        coroutineRun = true;
+        yield return new WaitForSeconds(5);
+        color.saturation.value -= 1;
+        coroutineRun = false;
     }
 }
