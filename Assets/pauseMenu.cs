8be5eb10f91f6016018ab1e1cac3947548b0160d
datsjustinc <jc9957@nyu.edu>
2022-05-08 03:01:00 -0400

    Implemented new start screen, start menu, and pause menu system.

---
+++ Assets/pauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject startMenuUI;
+    public static bool paused;
+    public AudioListener audioListener;
+
+    void start()
+    {
+        paused = false;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !startMenuUI.activeSelf)
+        {
+            if (paused)
+            {
+                Resume();
+            }
+
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Restart()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        SceneManager.LoadScene("Heist2");
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+        paused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        AudioListener.pause = true;
+        paused = true;
+    }
+}
---
+++ Assets/startMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startMenu : MonoBehaviour
+{
+    public GameObject startScreenUI;
+    public GameObject startVolume;
+    public GameObject outsideVolume;
+    public GameObject startCamera;
+    public GameObject mainCamera;
+    public GameObject player;
+   
+    public void exitStart()
+    {
+        startScreenUI.SetActive(false);
+        startVolume.SetActive(false);
+        outsideVolume.SetActive(true);
+        startCamera.SetActive(false);
+        mainCamera.SetActive(true);
+        player.SetActive(true);
+
+    }
+}
