85e20df29c03bcde643a7f56c607178a25966c67
datsjustinc <jc9957@nyu.edu>
2022-05-11 17:40:59 -0400

    Fixed spotlight obstacle collision respawn.

--- Assets/Scripts/BeamDetection.cs
+++ Assets/Scripts/BeamDetection.cs
@@ -7,19 +7,37 @@ public class BeamDetection : MonoBehaviour
     public GameObject spawn;
     public bool inside;
 
+    public StarterAssets.ThirdPersonController thirdPersonController;
+
     void Update()
     {
+        //Debug.Log(transform.position + "+" + spawn.transform.position);
+        /*
         if(inside)
         {
+            Debug.Log("Teleporting");
             transform.position = spawn.transform.position;
             inside = false;
         }
+        */
     }
     void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "camera")
+    { 
+        if(other.gameObject.CompareTag("camera")) 
         {
-            inside = true;
+            thirdPersonController.enabled = false;
+            transform.position = spawn.transform.position;
+            thirdPersonController.enabled = true;
+        }
+    }
+
+    void OnTriggerEnter (Collider other) 
+    {
+        if(other.gameObject.CompareTag("camera")) 
+        {
+            thirdPersonController.enabled = false;
+            transform.position = spawn.transform.position;
+            thirdPersonController.enabled = true;
         }
     }
 }
